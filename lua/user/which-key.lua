6503df450de84f3d1aae62f3bf6aa16f407c5722
弦間由樹 <gemma.naok@gmail.com>
2024-10-10 23:49:54 +0900

    Add which_key command related to Git

--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -1,9 +1,19 @@
 -- Git Keymaps
 lvim.builtin.which_key.mappings["g"] = {
     name = "Git",
+    a = { "<cmd>Gwrite<cr>", "Git Add" },
     b = { "<cmd>Telescope git_branches<cr>", "Git branches" },
+    B = { "<cmd>Git blame", "Git Brlame" },
     s = { "<cmd>Git status<cr>", "Git Status" },
+    S = { "<cmd>Gstatus<cr>", "Git Status" },
     c = { "<cmd>Git commit<cr>", "Git Commit" },
-    p = { "<cmd>Git push<cr>", "Git Push" },
+    p = { "<cmd>Git pull<cr>", "Git Pull" },
+    P = { "<cmd>Git push<cr>", "Git Push" },
+    r = { "<cmd>Gread<cr>", "Git Read(git checkout FILE)" },
     d = { "<cmd>Git diff<cr>", "Git Diff" },
+    l = { "<cmd>Gclog<cr>", "Git Log" },
+}
+
+lvim.builtin.which_key.mappings["f"] = {
+    "<cmd>Telescope find_files<cr>", "find files"
 }
